e17cac98c4d8d8b03772dadc7b93b6a9f1f3208c
zhihaoguo <zhihaoguo@microsoft.com>
2025-02-12 14:04:11 +0800

    init pr

---
+++ src/test/java/com/example/template_validation_action_sample/TemplateValidationActionSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.template_validation_action_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateValidationActionSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
